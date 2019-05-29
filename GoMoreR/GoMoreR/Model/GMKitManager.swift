a5c3821230c000047609c5fe79e16a06500cf12d
Sophie Liang <sophie.liang@bomdic.com>
2019-05-29 18:15:47 +0800

    refactor

--- GoMoreR/GoMoreR/Model/GMKitManager.swift
+++ GoMoreR/GoMoreR/Model/GMKitManager.swift
@@ -25,21 +25,21 @@ class GMKitManager {
                                     currentDateTime: Int(Date().timeIntervalSince1970))
         
         return GMKitManager.kit.initUser(age: (userData.birthday ?? Date()).age(),
-                                        gender: userData.gender == "male" ? 1: 0,
-                                        heightCm: userData.heightCm ?? 0,
-                                        weightKg: userData.weightKg ?? 0,
-                                        hrMax: userData.heartRateMax[0].heartRateMax ?? 0,
-                                        hrRest: userData.restingHeartRate ?? 0,
-                                        aerobicPtc: Float(workoutData.prevAerobicPtc ?? 100.0),
-                                        anaerobicPtc: Float(workoutData.prevAnaerobicPtc ?? 100.0),
-                                        staminaLevel: Float(workoutData.workoutInitDetail.filter { $0.typeId == .run }.first?.staminaLevel ?? 0.0),
-                                        teAerobic: 0,
-                                        teAnaerobic: 0,
-                                        teStamina: 0,
-                                        kcal: 0,
-                                        distance: 0,
-                                        elapsedSecond: Int(second),
-                                        checkSum: workoutData.workoutInitDetail.filter { $0.typeId == .run }.first?.checksum ?? "",
-                                        sportType: 31)
+                                         gender: userData.gender == "male" ? 1: 0,
+                                         heightCm: userData.heightCm ?? 0,
+                                         weightKg: userData.weightKg ?? 0,
+                                         hrMax: userData.heartRateMax[0].heartRateMax ?? 0,
+                                         hrRest: userData.restingHeartRate ?? 0,
+                                         aerobicPtc: Float(workoutData.prevAerobicPtc ?? 100.0),
+                                         anaerobicPtc: Float(workoutData.prevAnaerobicPtc ?? 100.0),
+                                         staminaLevel: Float(workoutData.workoutInitDetail.filter { $0.typeId == .run }.first?.staminaLevel ?? 0.0),
+                                         teAerobic: 0,
+                                         teAnaerobic: 0,
+                                         teStamina: 0,
+                                         kcal: 0,
+                                         distance: 0,
+                                         elapsedSecond: Int(second),
+                                         checkSum: workoutData.workoutInitDetail.filter { $0.typeId == .run }.first?.checksum ?? "",
+                                         sportType: 31)
     }
 }
--- GoMoreR/GoMoreR/Model/PublicEnum.swift
+++ GoMoreR/GoMoreR/Model/PublicEnum.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 enum UserDefaultsKey: String {
     case attribute
@@ -15,42 +14,8 @@ enum UserDefaultsKey: String {
     case user
 }
 
-extension Date {
-    
-    func age() -> Int {
-        let now = Date()
-        let calendar = Calendar.current
-        
-        let ageComponents = calendar.dateComponents([.year], from: self, to: now)
-        return ageComponents.year ?? 0
-    }
-    
-}
-
-public extension UIViewController {
-    
-    @discardableResult func showAlert(title: String?, message: String?, buttonTitles: [String]? = nil, highlightedButtonIndex: Int? = nil, completion: ((Int) -> Void)? = nil) -> UIAlertController {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        var allButtons = buttonTitles ?? [String]()
-        if allButtons.count == 0 {
-            allButtons.append("確認")
-        }
-        
-        for index in 0..<allButtons.count {
-            let buttonTitle = allButtons[index]
-            let action = UIAlertAction(title: buttonTitle, style: .default, handler: { (_) in
-                completion?(index)
-            })
-            alertController.addAction(action)
-            // Check which button to highlight
-            if let highlightedButtonIndex = highlightedButtonIndex, index == highlightedButtonIndex {
-                if #available(iOS 9.0, *) {
-                    alertController.preferredAction = action
-                }
-            }
-        }
-        present(alertController, animated: true, completion: nil)
-        return alertController
-    }
-    
+@objc enum UploadStatus: Int, Error {
+    case uploadFail = 0
+    case calculateFail = 1
+    case uploaded = 2
 }
