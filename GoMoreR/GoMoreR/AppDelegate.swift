421d45bb3996957e61eaae5a6cef29366bd89848
Sophie Liang <sophie.liang@bomdic.com>
2019-06-11 13:28:46 +0800

    feat: pod update and install Crashlytics

--- GoMoreR/GoMoreR/AppDelegate.swift
+++ GoMoreR/GoMoreR/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Intents
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        Fabric.with([Crashlytics.self])
         return true
     }
     
--- GoMoreR/Pods/Target Support Files/Pods-MessageIntents/Pods-MessageIntents-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MessageIntents : NSObject
-@end
-@implementation PodsDummy_Pods_MessageIntents
-@end
